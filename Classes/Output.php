badeb25bb421c2aeac7f690c35425d574a4155b1
ich <enslinger-m@gmx.de>
2019-01-11 10:12:13 +0100

    Update Output.php

--- Classes/Output.php
+++ Classes/Output.php
@@ -1,34 +1,55 @@
+
+<head>
+<link rel="stylesheet" href="LogIn.css">
+</head>
 <?php
 
 class Output{
-	
+
+	public function debug(){
+		$this->loginOutput(true);
+	}
+
 	public function indexOutput($variables){
-	
+
 		?>
-		
-		<h5>test</h5>
-		
-		
+
+
+
+
 		<?php
-	
+
 		$this->footerOutput();
 	}
-	
+
 	public function footerOutput(){
-		
+
 	}
-	
+
 	public function loginOutput($variables){
-	
+		?>
+		<div>
+			<div>
+		<h2>Login</h2>
+		</div>
+		<div>
+<p>Username</p><br><br>
+<p>Passwort</p>
+		</div>
+		<div>
+
+		</div>
+		</div>
+		<?php
 	}
-	
-		
+
+
 	public function registerOutput($variables){
-	
+
 	}
-	
+
 	public function gameBoardOutput($variables){
-		
+
 	}
-	
+
 }
