28d9d9a9b8d19a933b38be84ab194c0c3695864c
ich <enslinger-m@gmx.de>
2019-01-14 15:13:32 +0100

    output update

--- Classes/Output.php
+++ Classes/Output.php
@@ -1,6 +1,7 @@
 
 <head>
 <link rel="stylesheet" href="LogIn.css">
+<link
 </head>
 <?php
 
@@ -28,18 +29,19 @@ class Output{
 
 	public function loginOutput($variables){
 		?>
-		<div>
-			<div>
-		<h2>Login</h2>
+				<form id="loginform">
+					<div class="log">
+		<h2 class="logtext">Login</h2>
+		<label for="user"><b class="logtext">Benutzername:</b></label>
+		    <input type="text" placeholder="Benutzer eingeben" name="user" required>
+				<br>
+		    <label for="psw"><b class="logtext">Passwort:</b></label>
+		    <input type="password" placeholder="Passwort eingeben" name="psw" required><br>
+		    <button type="submit">Login</button><button onclick="window.location.href='/index.php?site=register'">Registrieren</button>
 		</div>
-		<div>
-<p>Username</p><br><br>
-<p>Passwort</p>
-		</div>
-		<div>
-
-		</div>
-		</div>
+	</form>
+	<script> src="quiz.js"</script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 		<?php
 	}
 
