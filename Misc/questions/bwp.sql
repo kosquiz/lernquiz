e030776651a2f1f3479a73577e0b7a2ec866c8b5
F.Stilo <F.Stilo@klara-oppenheimer-schule.de>
2019-05-20 14:40:03 +0200

    fragen

--- Misc/questions/bwp.sql
+++ Misc/questions/bwp.sql
@@ -1,10 +1,41 @@
 #fängt an mit 31
-INSERT INTO Question(Category, Question, Difficulty) VALUES('BWP', 'DrUMM', 1);
-INSERT INTO Question(Category, Question, Difficulty) VALUES('BWP', 'DrUMM', 2);
-INSERT INTO Question(Category, Question, Difficulty) VALUES('BWP', 'DrUMM', 3);
-INSERT INTO Question(Category, Question, Difficulty) VALUES('BWP', 'DrUMM', 4);
-INSERT INTO Question(Category, Question, Difficulty) VALUES('BWP', 'DrUMM', 5);
-INSERT INTO Question(Category, Question, Difficulty) VALUES('BWP', 'DrUMM', 1);
+INSERT INTO Question(Category, Question, Difficulty) VALUES('BWP', 'Wo setzt die Marktanalyse an', 1);
+INSERT INTO answer (Answer, Correct, Question_idQuestion) VALUES ("Zukunft", 0, 31);
+INSERT INTO answer (Answer, Correct, Question_idQuestion) VALUES ("Vergangenheit", 0, 31);
+INSERT INTO answer (Answer, Correct, Question_idQuestion) VALUES ("Zeitraum", 0, 31);
+INSERT INTO answer (Answer, Correct, Question_idQuestion) VALUES ("Zeitpunkt", 1, 31);
+
+INSERT INTO Question(Category, Question, Difficulty) VALUES('BWP', 'Was ist das Marktvolumen?', 2);
+INSERT INTO answer (Answer, Correct, Question_idQuestion) VALUES ("Mögliche Größe", 0, 32);
+INSERT INTO answer (Answer, Correct, Question_idQuestion) VALUES ("Theoretische Größe", 0, 32);
+INSERT INTO answer (Answer, Correct, Question_idQuestion) VALUES ("Anteil am Volumen", 0, 32);
+INSERT INTO answer (Answer, Correct, Question_idQuestion) VALUES ("Tatsächliche Größe", 1, 32);
+
+INSERT INTO Question(Category, Question, Difficulty) VALUES('BWP', 'Was gehört zu der Produktpolitik?', 3);
+INSERT INTO answer (Answer, Correct, Question_idQuestion) VALUES ("Handelsbetriebe", 0, 33);
+INSERT INTO answer (Answer, Correct, Question_idQuestion) VALUES ("Industriebetriebe", 0, 33);
+INSERT INTO answer (Answer, Correct, Question_idQuestion) VALUES ("Keins von beiden", 0, 33);
+INSERT INTO answer (Answer, Correct, Question_idQuestion) VALUES ("Industrie- u. Handelsbetriebe", 1, 33);
+
+INSERT INTO Question(Category, Question, Difficulty) VALUES('BWP', 'Der wievielte Ablaufpunkt des Produktlebenszyklus gehört die Sättigung?', 4);
+INSERT INTO answer (Answer, Correct, Question_idQuestion) VALUES ("2", 0, 34);
+INSERT INTO answer (Answer, Correct, Question_idQuestion) VALUES ("1", 0, 34);
+INSERT INTO answer (Answer, Correct, Question_idQuestion) VALUES ("5", 0, 34);
+INSERT INTO answer (Answer, Correct, Question_idQuestion) VALUES ("4", 1, 34);
+
+INSERT INTO Question(Category, Question, Difficulty) VALUES('BWP', 'Welches gehört nicht zur Preisdifferenzierung?', 5);
+INSERT INTO answer (Answer, Correct, Question_idQuestion) VALUES ("Zeitlich", 0, 35);
+INSERT INTO answer (Answer, Correct, Question_idQuestion) VALUES ("Persönlich", 0, 35);
+INSERT INTO answer (Answer, Correct, Question_idQuestion) VALUES ("Mengenmäßig", 0, 35);
+INSERT INTO answer (Answer, Correct, Question_idQuestion) VALUES ("Zusammenarbeitend", 1, 35);
+
+INSERT INTO Question(Category, Question, Difficulty) VALUES('BWP', 'Welches ist kein Werbeträger für ein Produkt?', 1);
+INSERT INTO answer (Answer, Correct, Question_idQuestion) VALUES ("Zeitung", 0, 35);
+INSERT INTO answer (Answer, Correct, Question_idQuestion) VALUES ("Fahrzeuge", 0, 35);
+INSERT INTO answer (Answer, Correct, Question_idQuestion) VALUES ("Internet", 0, 35);
+INSERT INTO answer (Answer, Correct, Question_idQuestion) VALUES ("Fernsehspot", 1, 35);
+
+
 INSERT INTO Question(Category, Question, Difficulty) VALUES('BWP', 'DrUMM', 2);
 INSERT INTO Question(Category, Question, Difficulty) VALUES('BWP', 'DrUMM', 3);
 INSERT INTO Question(Category, Question, Difficulty) VALUES('BWP', 'DrUMM', 4);
